1194dc9edacb9849d2d56778f98c2a7a438f14aa
Andrew <andrewalyuk@gmail.com>
2025-12-21 18:40:19 +0800

    added Bans and IPBans methods

---
+++ method_bans.go
@@ -0,0 +1,78 @@
+package gomcsmp
+
+import (
+	"context"
+
+	"github.com/eterline/go-mc-smp/internal/jsonrpc"
+	"github.com/eterline/go-mc-smp/internal/usage"
+)
+
+// Endpoints are accessible at minecraft:bans
+//
+// Path      | Description                      | Parameters            | Result
+// ----------|----------------------------------|-----------------------|----------------------
+// /         | Get the ban list                 | None                  | banlist: []UserBan
+// /set      | Set the banlist                  | bans: []UserBan       | banlist: []UserBan
+// /add      | Add players to the ban list      | add:  []UserBan       | banlist: []UserBan
+// /remove   | Remove players from ban list     | remove: []Player      | banlist: []UserBan
+// /clear    | Clear all players in ban list    | None                  | banlist: []UserBan
+
+// BansGet - Get the ban list
+func (rpc *RPCClient) BansGet(ctx context.Context) ([]UserBan, error) {
+	method := usage.NewMethod("bans").String()
+	r, err := rpc.core.CallWithContext(ctx, method, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := jsonrpc.DecodeRPCResult[[]UserBan](r)
+	if err != nil {
+		return nil, err
+	}
+
+	return *data, nil
+}
+
+// BansSet - Set the banlist
+func (rpc *RPCClient) BansSet(ctx context.Context, ban ...UserBan) error {
+	method := usage.NewMethod("bans").Add("set").String()
+	r, err := rpc.core.CallWithContext(ctx, method, []any{ban})
+	if err != nil {
+		return err
+	}
+
+	return r.Err()
+}
+
+// BansAdd - Add players to the ban list
+func (rpc *RPCClient) BansAdd(ctx context.Context, ban ...UserBan) error {
+	method := usage.NewMethod("bans").Add("add").String()
+	r, err := rpc.core.CallWithContext(ctx, method, []any{ban})
+	if err != nil {
+		return err
+	}
+
+	return r.Err()
+}
+
+// BansRemove - Remove players from ban list
+func (rpc *RPCClient) BansRemove(ctx context.Context, player ...Player) error {
+	method := usage.NewMethod("bans").Add("remove").String()
+	r, err := rpc.core.CallWithContext(ctx, method, []any{player})
+	if err != nil {
+		return err
+	}
+
+	return r.Err()
+}
+
+// BansClear - Clear all players in ban list
+func (rpc *RPCClient) BansClear(ctx context.Context) error {
+	method := usage.NewMethod("bans").Add("clear").String()
+	r, err := rpc.core.CallWithContext(ctx, method, nil)
+	if err != nil {
+		return err
+	}
+
+	return r.Err()
+}
---
+++ method_ipbans.go
@@ -0,0 +1,78 @@
+package gomcsmp
+
+import (
+	"context"
+
+	"github.com/eterline/go-mc-smp/internal/jsonrpc"
+	"github.com/eterline/go-mc-smp/internal/usage"
+)
+
+// Endpoints are accessible at minecraft:ip_bans
+//
+// Path      | Description                      | Parameters            | Result
+// ----------|----------------------------------|-----------------------|----------------------
+// /         | Get the ip ban list              | None                  | banlist: []IPBan
+// /set      | Set the ip ban list              | bans: []IPBan         | banlist: []IPBan
+// /add      | Add players to the ip ban list   | add:  []IPBan         | banlist: []IPBan
+// /remove   | Remove players from ip ban list  | remove: []Player      | banlist: []IPBan
+// /clear    | Clear all players in ip ban list | None                  | banlist: []IPBan
+
+// IPBansGet - Get the ip ban list
+func (rpc *RPCClient) IPBansGet(ctx context.Context) ([]IPBan, error) {
+	method := usage.NewMethod("ip_bans").String()
+	r, err := rpc.core.CallWithContext(ctx, method, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := jsonrpc.DecodeRPCResult[[]IPBan](r)
+	if err != nil {
+		return nil, err
+	}
+
+	return *data, nil
+}
+
+// IPBansSet - Set the ip ban list
+func (rpc *RPCClient) IPBansSet(ctx context.Context, ban ...IPBan) error {
+	method := usage.NewMethod("ip_bans").Add("set").String()
+	r, err := rpc.core.CallWithContext(ctx, method, []any{ban})
+	if err != nil {
+		return err
+	}
+
+	return r.Err()
+}
+
+// IPBansAdd - Add players to the ip ban list
+func (rpc *RPCClient) IPBansAdd(ctx context.Context, ban ...IPBan) error {
+	method := usage.NewMethod("ip_bans").Add("add").String()
+	r, err := rpc.core.CallWithContext(ctx, method, []any{ban})
+	if err != nil {
+		return err
+	}
+
+	return r.Err()
+}
+
+// IPBansRemove - Remove players from ip ban list
+func (rpc *RPCClient) IPBansRemove(ctx context.Context, player ...IPBan) error {
+	method := usage.NewMethod("ip_bans").Add("remove").String()
+	r, err := rpc.core.CallWithContext(ctx, method, []any{player})
+	if err != nil {
+		return err
+	}
+
+	return r.Err()
+}
+
+// IPBansClear - Clear all players in ip ban list
+func (rpc *RPCClient) IPBansClear(ctx context.Context) error {
+	method := usage.NewMethod("ip_bans").Add("clear").String()
+	r, err := rpc.core.CallWithContext(ctx, method, nil)
+	if err != nil {
+		return err
+	}
+
+	return r.Err()
+}
